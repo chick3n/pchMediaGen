77cf16fd09e600b26794a5327e1b70b2d2949559
chick3n <chick3n@gmail.com>
2013-04-06 16:39:05 -0400

    fixed ignore

--- popcorn.sql
+++
@@ -1,3 +0,0 @@
-CREATE TABLE "tv" ("tv_id" INTEGER PRIMARY KEY  AUTOINCREMENT  NOT NULL , "tv_name" VARCHAR, "update" DATETIME, "last_update" DATETIME);
-CREATE TABLE "tv_episode" ("tvepisode_id" INTEGER PRIMARY KEY  AUTOINCREMENT  NOT NULL , "episode_name" VARCHAR, "episode_number" VARCHAR, "episode_description" VARCHAR, "raw_file_name" VARCHAR, "updated_on" DATETIME, "last_update_html" DATETIME);
-CREATE TABLE "tv_season" ("tvseason_id" INTEGER PRIMARY KEY  AUTOINCREMENT  NOT NULL , "season_name" VARCHAR, "updated_on" , "last_update" , "tv_id" INTEGER NOT NULL );
