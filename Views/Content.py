b7cfd934171b622ffb9ce848f8029db1bf6e8a18
chicken <chick3n@gmail.com>
2013-11-18 06:18:36 -0500

    *

--- Views/Content.py
+++ Views/Content.py
@@ -6,9 +6,10 @@ from math import ceil, floor
 import cgi
 from Filter import Filter
 
+
 class Content(object):
     def __init__(self):
-        super(Content,self).__init__()
+        super(Content, self).__init__()
         self.db = "../media.sqlite"
         self.sql = db.MultiThreadOK(self.db)
 
@@ -44,11 +45,11 @@ class Content(object):
 
         delta = datetime.today() - added
         if delta.days > 365:
-            return "%s y" % int(floor(delta.days / 365))
+            return "%s year" % int(floor(delta.days / 365))
         elif delta.days > 30:
-            return "%s m" % int(floor(delta.days / 30))
+            return "%s month" % int(floor(delta.days / 30))
 
-        return "%s d" % delta.days
+        return "%s day" % delta.days
 
     def convert_watched_to_dayssince(self, watched):
         if watched is None:
@@ -65,13 +66,13 @@ class Content(object):
 
         delta = datetime.now() - watched
         if delta.days > 365:
-            return "%s y" % int(floor(delta.days / 365))
+            return "%s year" % int(floor(delta.days / 365))
         elif delta.days > 30:
-            return "%s m" % int(floor(delta.days / 30))
+            return "%s month" % int(floor(delta.days / 30))
 
-        return "%s d" % delta.days
+        return "%s day" % delta.days
 
-    def get_available_letters(self, _from, _select, _joins = None, _filters = None, _groups = None, _limits = None):
+    def get_available_letters(self, _from, _select, _joins=None, _filters=None, _groups=None, _limits=None):
         sql = "select %s from %s" % (_select, _from)
 
         if _joins is not None:
@@ -88,11 +89,12 @@ class Content(object):
                 continue
             if result[0][0].isalpha():
                 letters.add(result[0][0].upper())
-            else: letters.add('#')
+            else:
+                letters.add('#')
 
         return sorted(letters)
 
-    def get_total_items(self, _from, _select, _joins = None, _filters = None, _groups = None, _limits = None):
+    def get_total_items(self, _from, _select, _joins=None, _filters=None, _groups=None, _limits=None):
         sql = "select count(%s) from %s" % (_select, _from)
 
         if _joins is not None:
@@ -110,20 +112,20 @@ class Content(object):
 
         return total
 
-    def get_next_page(self, page, limit, column, total = None):
-        if page >= 0 and ((page + 1)*limit) <= total:
-            return page+1
+    def get_next_page(self, page, limit, column, total=None):
+        if page >= 0 and ((page + 1) * limit) <= total:
+            return page + 1
 
         return 0
 
-    def get_prev_page(self, page, limit, column, total = None):
+    def get_prev_page(self, page, limit, column, total=None):
         if (page - 1) >= 0 and ((page - 1) * limit) <= total:
             return page - 1
 
         return int(ceil(total / limit))
 
 
-    def get_latest_tv_html(self, page = 0, limit = 5, filter = None):
+    def get_latest_tv_html(self, page=0, limit=5, filter=None):
         episodes = self.get_latest_tv(page, limit, filter)
 
         html = []
@@ -132,7 +134,8 @@ class Content(object):
             show = episode["show"]
             if episode["episode"] is not None and int(episode["episode"]) < 10:
                 episode["episode"] = "0" + str(episode["episode"])
-            title = episode["file_name"] if episode["title"] is None else "%s. %s" % (episode["episode"], episode["title"])
+            title = episode["file_name"] if episode["title"] is None else "%s. %s" % (
+                episode["episode"], episode["title"])
             desc = episode["desc"] if not episode["desc"] is None else ""
             path = "file:///opt/sybhttpd/localhost.drives/NETWORK_SHARE/mediaui/Tv/%s" % (episode["full_path"])
             fanart = episode["fanart"] if not episode["fanart"] is None else "tv_fanart.png"
@@ -168,14 +171,14 @@ class Content(object):
                          , watcheddate
                          , episode["id"]
                          , fulltitle
-                ))
+            ))
 
         return html
 
-    def get_latest_tv_added(self, limit = 5):
+    def get_latest_tv_added(self, limit=5):
         pass
 
-    def get_latest_tv(self, page = 0, limit = 5, _filter = None):
+    def get_latest_tv(self, page=0, limit=5, _filter=None):
         sql = """
             select s.title, e.season, e.episode, e.title, e.full_path, e.file_name, e.description, e.added
             ,s.fanart, s.banner, s.poster, datetime(e.watched, 'localtime'), e.id
@@ -191,7 +194,7 @@ class Content(object):
             order by added desc
             limit ?, ?"""
 
-        episodes = self.sql.select(sql,(page*limit,limit,))
+        episodes = self.sql.select(sql, (page * limit, limit,))
         content = []
 
         for episode in episodes:
@@ -228,7 +231,7 @@ class Content(object):
         else:
             return
 
-        self.sql.execute(sql,(id,))
+        self.sql.execute(sql, (id,))
         self.sql.execute("COMMIT")
 
         return
@@ -274,10 +277,9 @@ class Content(object):
         if episode["title"].__len__() > 30:
             episode["title"] = episode["title"][:30] + "..."
 
-
         return episode
 
-    def get_tv_series_html(self, show, season = None, page = 0, limit = 5, filter = None):
+    def get_tv_series_html(self, show, season=None, page=0, limit=5, filter=None):
         episodes = self.get_tv_series(show, season, page, limit, filter)
 
         html = []
@@ -286,7 +288,8 @@ class Content(object):
             show = episode["show"]
             if episode["episode"] is not None and int(episode["episode"]) < 10:
                 episode["episode"] = "0" + str(episode["episode"])
-            title = episode["file_name"] if episode["title"] is None else "%s. %s" % (episode["episode"], episode["title"])
+            title = episode["file_name"] if episode["title"] is None else "%s. %s" % (
+                episode["episode"], episode["title"])
             desc = episode["desc"] if not episode["desc"] is None else ""
             path = "file:///opt/sybhttpd/localhost.drives/NETWORK_SHARE/mediaui/Tv/%s" % (episode["full_path"])
             fanart = episode["fanart"] if not episode["fanart"] is None else "tv_fanart.png"
@@ -322,11 +325,11 @@ class Content(object):
                          , watcheddate
                          , episode["id"]
                          , fulltitle
-                ))
+            ))
 
         return html
 
-    def get_tv_series(self, show, season = None, page = 0, limit = 5, filter = None):
+    def get_tv_series(self, show, season=None, page=0, limit=5, filter=None):
         sql = """
                 select s.title, e.season, e.episode, e.title, e.full_path, e.file_name, e.description, e.added
                 ,s.fanart, s.banner, s.poster, datetime(e.watched, 'localtime'), e.id
@@ -340,7 +343,7 @@ class Content(object):
 
         sql += " order by e.episode asc limit ?, ?"
 
-        episodes = self.sql.select(sql,((page * limit), limit,))
+        episodes = self.sql.select(sql, ((page * limit), limit,))
         content = []
 
         for episode in episodes:
@@ -363,7 +366,7 @@ class Content(object):
 
         return content
 
-    def get_tv_shows_html(self, page = 0, limit = 5, f = None):
+    def get_tv_shows_html(self, page=0, limit=5, f=None):
         sql = """
                 SELECT s.title, s.fanart
                 , (select count(e.id) from episode e where e.show = s.id)
@@ -379,7 +382,7 @@ class Content(object):
 
         sql += " ORDER BY s.title ASC LIMIT ?, ?"
 
-        results = self.sql.select(sql, (page*limit, limit,))
+        results = self.sql.select(sql, (page * limit, limit,))
 
         content = {
             "shows": []
@@ -415,7 +418,7 @@ class Content(object):
             """
         sql2 = "select count(e.id) from episode e where e.show = ? and e.season is null"
 
-        results = self.sql.select(sql, (id, page*limit, limit,))
+        results = self.sql.select(sql, (id, page * limit, limit,))
 
         content = []
         for result in results:
@@ -465,18 +468,18 @@ class Content(object):
 
         for result in results:
             content["show"] = {
-                    "fulltitle": result[0] #title
-                    , "fanart": result[1] if result[1] is not None else "tv_fanart.png" #fanart
-                    , "epscount": result[2] #episode count
-                    , "shorttitle": result[0][:30]
-                    , "seasons": sorted(result[3].split(","), key=int)
-                    , "id": result[4]
-                    , "lastadded": self.convert_date_to_dayssince(result[5])
-                }
+                "fulltitle": result[0] #title
+                , "fanart": result[1] if result[1] is not None else "tv_fanart.png" #fanart
+                , "epscount": result[2] #episode count
+                , "shorttitle": result[0][:30]
+                , "seasons": sorted(result[3].split(","), key=int)
+                , "id": result[4]
+                , "lastadded": self.convert_date_to_dayssince(result[5])
+            }
 
         return content
 
-    def get_movies(self, page = 0, limit = 5, filter = None, order = 'm.title ASC'):
+    def get_movies(self, page=0, limit=5, filter=None, order='m.title ASC'):
         sql = "select m.title, m.description, m.airdate, m.full_path, m.parent_dir, m.added, m.id, datetime(m.watched, 'localtime'), m.runtime, m.poster from movie m"
 
         if filter is not None:
@@ -511,3 +514,71 @@ class Content(object):
             html.append(content)
 
         return html
+
+    def get_all_media_content(self, page=0, limit=50, order='date', direction='desc'):
+        sql = """select * from
+                (select
+                   episode.id, episode.title, episode.added,
+                   show.title as extraTitle,
+                   'tv' as type
+                from episode
+                inner join show on show.id = episode.show
+
+                union all
+
+                select
+                   movie.id, movie.title, movie.added, '' as extraTitle, 'movie' as type
+                from movie)
+                order by %s %s limit ?, ? """ % ('added' if order == 'date' else 'title',
+                                                 'desc' if direction == 'desc' else 'asc')
+
+        mediaContent = self.sql.select(sql, (page * limit, limit,))
+
+        results = []
+
+        for media in mediaContent:
+            content = {}
+            content['id'] = media[0]
+            content['title'] = media[1] if media[1] is not None else ""
+            content['dayssince'] = self.convert_watched_to_dayssince(media[2])
+            content['added'] = media[2]
+            content['extra'] = media[3] if media[3] is not None else ""
+            content['type'] = media[4]
+            results.append(content)
+
+        return results
+
+    def get_movie(self, mediaid):
+        sql = """select m.title, m.description, m.airdate, m.full_path,
+                    m.parent_dir, m.added, m.id, datetime(m.watched, 'localtime'),
+                    m.runtime, m.poster, m.file_name, m.imdbid
+                    from movie m
+                        where m.id = ?"""
+
+        movies = self.sql.select(sql, (int(mediaid),))
+
+        content = {}
+        for movie in movies:
+            content["id"] = movie[6]
+            content["title"] = movie[0] if movie[0] is not None else movie[4]
+            content["desc"] = movie[1] if not movie[1] is None else ""
+            content["path"] = "file:///opt/sybhttpd/localhost.drives/NETWORK_SHARE/mediaui/Movies/%s" % (movie[3])
+            content["fanart"] = "/images/movie/" + movie[9] if movie[9] is not None else None
+            content["added"] = self.convert_date_to_dayssince(movie[5])
+            content["watchedicon"] = "/images/unwatched.png" if movie[7] is None else "/images/watched.png"
+            content["watcheddate"] = self.convert_watched_to_dayssince(movie[7])
+            content["fulltitle"] = content["title"]
+            content["runtime"] = self.convert_runtime_seconds(movie[8])
+            content["released"] = self.strip_year_from_date(movie[2])
+            content["filename"] = movie[10]
+            content["imdb"] = movie[11]
+
+            if content["desc"].__len__() > 300:
+                content["desc"] = content["desc"][:300] + "..."
+
+            if content["title"].__len__() > 30:
+                content["title"] = content["title"][:30] + "..."
+
+            return content
+
+        return None
